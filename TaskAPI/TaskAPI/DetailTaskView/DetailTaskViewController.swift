decda9387cc8c3b4ba351880922c714879052fae
juokauju <justina.siaulyte@protonmail.com>
2023-03-02 20:51:25 +0200

    add DetailTaskViewController

--- TaskAPI/TaskAPI/DetailTaskView/DetailTaskViewController.swift
+++ TaskAPI/TaskAPI/DetailTaskView/DetailTaskViewController.swift
@@ -53,7 +53,7 @@ extension DetailTaskViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseId, for: indexPath)
         
-        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
--- TaskAPI/TaskAPI/DetailTaskView/StepperTableViewCell.swift
+++ TaskAPI/TaskAPI/DetailTaskView/StepperTableViewCell.swift
@@ -9,15 +9,8 @@ import UIKit
 
 class StepperTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    let reuseId = "StepperTableViewCell"
+    
+    
 
 }
